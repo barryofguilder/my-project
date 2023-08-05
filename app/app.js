f1e3ade016087acc74a679779890131ef74a5f2e
Jason Barry <jbarry@hey.com>
2023-08-05 18:13:14 -0400

    Go through steps on Tailwind site

--- app/app.js
+++ app/app.js
@@ -3,6 +3,12 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'my-project/config/environment';
 
+// This will throw an error.
+// import 'my-project/app.css';
+
+// This is what works.
+import 'my-project/assets/my-project.css';
+
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
